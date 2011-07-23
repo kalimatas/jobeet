384d78eac51389c9c918888325588e872ddacf5d
Guz Alex <kalimatas@gmail.com>
2011-07-23 17:45:34 +0400

    Zend_Lucene

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -5,9 +5,23 @@ sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
-  public function setup()
-  {
-    $this->enablePlugins('sfDoctrinePlugin');
-    $this->enablePlugins('sfDoctrineGuardPlugin');
-  }
+    static protected $zendLoaded = false;
+
+    public function setup()
+    {
+        $this->enablePlugins('sfDoctrinePlugin');
+        $this->enablePlugins('sfDoctrineGuardPlugin');
+    }
+
+    static public function registerZend()
+    {
+        if ( self::$zendLoaded ) {
+            return;
+        }
+
+        set_include_path(sfConfig::get('sf_lib_dir').'/vendor'.PATH_SEPARATOR.get_include_path());
+        require_once sfConfig::get('sf_lib_dir').'/vendor/Zend/Loader/Autoloader.php';
+        Zend_Loader_Autoloader::getInstance();
+        self::$zendLoaded = true;
+    }
 }
--- lib/task/JobeetCleanupTask.class.php
+++ lib/task/JobeetCleanupTask.class.php
@@ -24,6 +24,24 @@ EOF;
     {
         $databaseManager = new sfDatabaseManager($this->configuration);
 
+        // очистка индекса Lucene
+        $index = JobeetJobTable::getLuceneIndex();
+
+        $q = Doctrine_Query::create()
+            ->from('JobeetJob j')
+            ->where('j.expires_at < ? ', date('Y-m-d'));
+
+        $jobs = $q->execute();
+        foreach ( $jobs as $job ) {
+            if ( $hit = $index->find('pk:'.$job->getId()) ) {
+                $index->delete($hit->id);
+            }
+        }
+
+        $index->optimize();
+        $this->logSection('lucene', 'Cleaned up and optimized the job index.');
+
+        // удаляем устаревшие вакансии
         $nb = Doctrine_Core::getTable('JobeetJob')->cleanup($options['days']);
         $this->logSection('doctrine', sprintf('Removed %d stale jobs', $nb));
     }
